0d9a41ff937102b5868c739c949463b4d967f53f
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-07-27 02:13:42 +0530

    added dummy data

---
+++ Backend/data/data.js
@@ -0,0 +1,110 @@
+  const chats = [
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Tishu",
+        email: "Tishu@example.com",
+      },
+      {
+        name: "Arin",
+        email: "Arin@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7c4dd4",
+    chatName: "Tishu",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+      {
+        name: "Arin",
+        email: "Arin@example.com",
+      },
+    ],
+    _id: "617a077e18c25468b27c4dd4",
+    chatName: "Arin",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Anni",
+        email: "Anni@example.com",
+      },
+      {
+        name: "Arin",
+        email: "Arin@example.com",
+      },
+    ],
+    _id: "617a077e18c2d468bc7c4dd4",
+    chatName: "Anni",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "Tishu",
+        email: "Tishu@example.com",
+      },
+      {
+        name: "Arin",
+        email: "Arin@example.com",
+      },
+      {
+        name: "Ani",
+        email: "Ani@example.com",
+      },
+    ],
+    _id: "617a518c4081150716472c78",
+    chatName: "Roomies",
+    groupAdmin: {
+      name: "Ani",
+      email: "Ani@example.com",
+    },
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Arin",
+        email: 'Arin@example.com',
+      },
+      {
+        name: "Tishu",
+        email: "Tishu@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7cfdd4",
+    chatName: "Tishuuuu",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "Khanna",
+        email: "Khanna@example.com",
+      },
+      {
+        name: "Partyas",
+        email: "Partyas@example.com",
+      },
+      {
+        name: "Attri",
+        email: "Attri@example.com",
+      },
+    ],
+    _id: "617a518c4081150016472c78",
+    chatName: "Fukre",
+    groupAdmin: {
+      name: "Partyas",
+      email: "partyas@example.com",
+    },
+  },
+];
+
+module.exports = {chats};
