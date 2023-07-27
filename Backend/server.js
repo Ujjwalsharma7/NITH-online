a112bde6355fba5556a603bb2e7c4d5dc56711c5
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-07-27 14:44:26 +0530

    sending sibgle chats through id

--- Backend/server.js
+++ Backend/server.js
@@ -11,4 +11,9 @@ app.get('/api/chat', (req,res) => {
     res.send(chats);
 })
 
+app.get("/api/chat/:id", (req,res) => {
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+})
+
 app.listen(3000, console.log("Server started on port 3000"));
