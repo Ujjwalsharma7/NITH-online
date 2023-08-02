8e5052ae516f16cee18dd7aa2fe7da3ef0c3569d
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-08-02 15:28:57 +0530

    added layout for signup and login form

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -15,10 +15,10 @@ const ChatPage = () => {
 
   return (
     <div>
-      {chats.map((chat) => (
-        <div key={chat._id}>{chat.chatName}</div>
+      {/* {chats.map((chat) => (
+        // <div key={chat._id}>{chat.chatName}</div>
       ))}
-      ;
+      ; */}
     </div>
   );
 };
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,50 @@
 import React from "react";
+import {
+  Container,
+  Box,
+  Text,
+  Tab,
+  Tabs,
+  TabList,
+  TabPanel,
+  TabPanels,
+} from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 
 const HomePage = () => {
-  return <div>Homeeeeee</div>;
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        display="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderWidth="1px"
+        fontWeight={500}
+      >
+        <Text>NITH-ONLINE</Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs variant="soft-rounded" colorScheme="green">
+          <TabList>
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Signup</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default HomePage;
